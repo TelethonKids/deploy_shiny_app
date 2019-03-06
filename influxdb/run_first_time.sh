84d396a0a82d85fce5a46b3a35d91fa9e1de9e44
pstevenson <Paul.Stevenson@telethonkids.org.au>
2019-03-06 12:20:34 +0800

    This update closes #2 and closes #3

--- influxdb/run_first_time.sh
+++ influxdb/run_first_time.sh
@@ -3,7 +3,7 @@
 # Author : Paul Stevenson
 # Create influxdb config file and database for shinyproxy usage statistics
 # only to be run once after the first "docker-compose up -d" on a system
-# run in docker with: docker exec influxdb /home/run_first_time.sh
+# run in docker with: docker exec tki_influxdb sh /home/run_first_time.sh
 
 # influxdb configuration file creation
 influxd config > influxdb.conf
